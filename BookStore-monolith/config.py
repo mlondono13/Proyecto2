bdd5a472d3a1e35dd4ea0dec52bdc9ebf91bb744
Marcela Londoño Leon <104228951+mlondono13@users.noreply.github.com>
2025-05-12 19:39:40 -0500

    Update config.py

--- BookStore-monolith/config.py
+++ BookStore-monolith/config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin123@database-1.ctkwuucka690.us-east-1.rds.amazonaws.com'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
